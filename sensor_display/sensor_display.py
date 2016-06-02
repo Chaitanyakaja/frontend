548623cffa275f6cdcbd44e4890ce0aa1dc686ac
maddz131 <maddz131@gmail.com>
2016-06-02 13:02:57 -0700

    Flask app takes readings from pH DO and temp sensors and displays them

--- sensor_display/sensor_display.py
+++ sensor_display/sensor_display.py
@@ -3,25 +3,43 @@ from flask import render_template, redirect, url_for
 #from apscheduler.scheduler import Scheduler #when I want it to update data automatically
 import sys
 import serial
+from pyduino import *
 import time
+from array import array
 
 app = Flask(__name__)
 
-#opens serial connection to arduino
+#opens serial connection to arduino/raspberry pi
 #usbport = '/dev/cu.usbmodemfa131'
 usbport = '/dev/cu.usbmodemfd121'
-ser = serial.Serial(usbport,9600)
+ser = serial.Serial(usbport,9600) #note the use of usbport variable
 
 sensors = {
-	1 : {'name' : 'Temp', 'command' : 't', 'reading' : 0},
-	2 : {'name' : 'pH', 'command' : 'h', 'reading' : 0}
+	0 : {'name' : 'pH', 'command' : '0:r', 'reading' : 'N/A'},
+	1 : {'name' : 'Temp', 'command' : '1:r', 'reading' : 'N/A'},
+	2 : {'name' : 'DO', 'command' : '2:r', 'reading' : 'N/A'},
+	#3 : {'name' : 'Humidity', 'command' : '2:r', 'reading' : 'N/A'}
+	#3 : {'name' : 'temp', 'command' : ['1','2','3'], 'reading' : 'N/A'},
+#	2 : {'name' : 'temp_k', 'command' : '1:f', 'reading' : 'N/A'},
+#	2 : {'name' : 'Humidity', 'command' : 'h', 'reading' : 'N/A'}
 }
 
+config = {
+	1: {'response' : '1:response\r'},
+}
 def get_updates():
+	for num in config:
+		ser.write(config[num]['response'])
+
 	for num in sensors:
-			ser.write(sensors[num]['command'])
+		ser.write(sensors[num]['command'])
+		if ser.readline() > 0:
 			reading = ser.readline()
-			sensors[num]['reading'] = reading
+		#else 'N/A'
+		#reading = ser.readline()
+		sensors[num]['reading'] = reading
+		time.sleep(1)
+
 	return 0
 
 @app.route('/')
@@ -38,4 +56,4 @@ def update():
 	return redirect(url_for('index'))
 
 
-app.run(debug = True, host = '0.0.0.0', port = 8000)
+app.run(debug = True, host = '0.0.0.0', port = 5000)
